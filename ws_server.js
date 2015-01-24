97617e5ccf3032f720a0005807587d03703117f6
Andrew Kuznetcov <bilberry.pie@gmail.com>
2015-01-24 17:36:04 +0300

    Broadcasting

--- ws_server.js
+++ ws_server.js
@@ -1,10 +1,16 @@
-var ws = require("nodejs-websocket")
+var ws = require("nodejs-websocket");
+
+function broadcast(server, msg) {
+  server.connections.forEach(function (conn) {
+    conn.sendText(msg)
+  })
+}
 
 var server = ws.createServer(function(conn) {
     console.log("New connection")
     conn.on("text", function(str) {
-        console.log("Received "+str)
-        conn.sendText(new Date().toISOString() + ", Anonymous: "+str)
+        console.log("Received "+str);
+        broadcast(server,new Date().toISOString() + ", Anonymous: "+str.replace(/</g,'&#60;').replace(/>/g,'&#62;'));
     })
     conn.on("close", function(code, reason) {
         console.log("Connection closed")
